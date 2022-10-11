330ab51c7d35cf063d4f984c8034afdf93dc8ef8
Karthik <56579449+karthik9728@users.noreply.github.com>
2022-10-11 12:12:39 +0530

    Create Category

---
+++ Abby/AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,30 @@
+using AbbyWeb.Data;
+using AbbyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _db.Categories.AddAsync(Category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}
